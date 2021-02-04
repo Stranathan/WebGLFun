cea17ff99d5189ae9285bf69016f80994165a92a
Stranathan <ian.p.stranathan@gmail.com>
2021-02-04 05:26:39 -0800

    fixed merge sort

--- algorithmAnimations/algos/sortingAlgorithms.js
+++ algorithmAnimations/algos/sortingAlgorithms.js
@@ -17,6 +17,7 @@ function mergeSort(anUnsortedArray)
 
     // const behave like let variables, except they cannot be reassigned:
     const theMiddleIndex = Math.floor(anUnsortedArray.length / 2);
+    //console.log("the middle index is: " + theMiddleIndex);
     const leftHalf = anUnsortedArray.slice(0, theMiddleIndex); // slice:: ends at, but does not include, the given end argument.
     const rightHalf = anUnsortedArray.slice(theMiddleIndex); // slice:: If end is omitted, slice extracts through the end of the sequence (arr.length).
     
@@ -31,51 +32,35 @@ function merge(lh, rh)
     let ri = 0; // the index of the right array
     let li = 0; // the index of the left array
     let m = []; // the merged array
+    lhLen = lh.length;
+    rhLen = rh.length;
 
-    // linear time merge
-    for(let i = 0; i < lh.length + rh.length; i++)
+    for(let i = 0; i < lhLen + rhLen; i++)
     {
-        // ---- This is for case where we're merging 
-        if(ri == rh.length && li == lh.length - 1)
-        {
-            m.push(lh[li]);
-        }
-        else if(li == lh.length && ri == rh.length - 1)
+        // collect the last element remaining
+        if(li == lhLen)
         {
             m.push(rh[ri]);
+            ri++;
         }
-        else if(ri == rh.length && li == lh.length - 2)
+        else if(ri == rhLen)
         {
-            if(lh[li] < lh[li + 1])
-            {
-                m.push(lh[li]);
-                m.push(lh[li + 1]);
-                break;
-            }
-        }
-        else if(li == lh.length && ri == rh.length - 2)
-        {
-            if(rh[ri] < rh[ri + 1])
-            {
-                m.push(rh[ri]);
-                m.push(rh[ri + 1]);
-                break;
-            }
+            m.push(lh[li]);
+            li++;
         }
         else
         {
             if(lh[li] < rh[ri])
             {
                 m.push(lh[li]);
-                li += 1;
+                li++;
             }
             else
             {
                 m.push(rh[ri]);
-                ri += 1;
+                ri++;
             }
         }
     }
-    //mergeCount++;
     return m;
 }
